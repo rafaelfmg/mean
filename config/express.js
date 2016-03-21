b1dfe98a97a35303e2308300ed77c0793b9027ef
Rafael Faria <rafaelf.fmg@gmail.com>
2016-03-21 11:19:35 -0300

    Inclusão codigo

---
+++ config/express.js
@@ -0,0 +1,23 @@
+var express = require('express');
+/*var home = require('../app/routes/home');*/
+var load = require('express-load');
+var bodyParser = require('body-parser');
+module.exports = function() {
+var app = express();
+app.set('port', 3000);
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+app.set('views','./app/views');
+// novos middlewares
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(require('method-override')());
+
+/*home(app);*/
+load('models', {cwd: 'app'})
+.then('controllers')
+.then('routes')
+.into(app);
+
+return app;
+};
